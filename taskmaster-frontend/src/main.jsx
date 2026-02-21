2f00b9b9fd77dad8ffe6d97f348e047279d87f19
odavilla200109-oss <odavilla200109@gmail.com>
2026-02-20 21:11:55 -0300

    frontend taskmaster

---
+++ taskmaster-frontend/src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import App from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+    <App />
+  </GoogleOAuthProvider>
+);
